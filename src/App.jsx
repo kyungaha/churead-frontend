5393b15b4fe006d4da81b867ced43ee3ed547db9
kyungaha <buzzka91@naver.com>
2025-07-16 15:52:41 +0900

    OAuth 사용 로그인(구글로그인)

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,30 @@ import Post from "./pages/Post";
 import Profile from "./pages/Profile";
 import Edit from "./pages/Edit";
 import Sample from "./pages/Sample";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase";
 
 function App() {
   // logic
+  //진입시에는 무조건 로딩, 인증서비스 준비 완료 후에 로딩 false로 변경
+  const [isLoading, setIsLoading] =useState(true);
+
+  const init = async() => {
+    // firebase에서 로그인 데이터를 가져옴
+    await auth.authStateReady() // 로그인 상태 변화 감지
+    console.log("인증완료", auth);
+    setIsLoading(false);
+  }
+
+  // 페이지 진입 후 딱 한 번 실행함
+  useEffect(() => {
+    init()
+  }, [])
 
   // view
   return (
     <div className="bg-churead-black h-full text-white overflow-auto">
-      <div className="max-w-[572px] mx-auto h-full">
+      {isLoading ? <p className="text-2xl">Loading...</p> : <div className="max-w-[572px] mx-auto h-full">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -25,7 +41,8 @@ function App() {
             <Route path="/sample" element={<Sample />} />
           </Routes>
         </BrowserRouter>
-      </div>
+      </div>}
+      
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import InputField from "../components/InputField";
 import LoginButton from "../components/LoginButton";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
   // logic
+  const history = useNavigate()
 
   const handleInputChange = (inputValue, field) => {
     // TODO: 사용자 입력 기능 구현
@@ -14,8 +17,18 @@ const Login = () => {
     // TODO: 로그인 기능 구현
   };
 
-  const handleGoogleLogin = () => {
+  const handleGoogleLogin = async () => {
     // TODO: 구글 로그인 구현
+    console.log("구글 로그인!!");
+    const provider = new GoogleAuthProvider();
+    try {
+      // 1. 팝업 띄워서 로그인
+      await signInWithPopup(auth, provider);
+      // 2. 홈 화면으로 리다이렉트
+      history("/");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   // view
