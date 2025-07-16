1785706d552017c6f61eb6618a3192f7fd060e03
kyungaha <buzzka91@naver.com>
2025-07-16 14:25:25 +0900

    react 실습2

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Greeting from '../components/sample/Greeting'
 
 const Sample = () => {
@@ -6,13 +6,21 @@ const Sample = () => {
     const handeleButtonClick= (data) => {
         console.log("click!", data);
     }
+
+    const [userNames, setUserNames] = useState([]);
+    useEffect(() => {
+        // 컴포넌트 생성 시 딱 한번만 실행
+        const nameDatas = ["kong", "하경아", "kyunga"];
+        setUserNames(nameDatas);
+    }, []);
     
     // view
     return (
     <div>Sample
-        <Greeting name="kong" onButtonClick={handeleButtonClick}/>
+        {userNames.map((userName, index) => <Greeting key={`userName${index}`} name={userName} onButtonClick={handeleButtonClick}/>)}
+        {/* <Greeting name="kong" onButtonClick={handeleButtonClick}/>
         <Greeting name="하경아" onButtonClick={handeleButtonClick}/>
-        <Greeting onButtonClick={handeleButtonClick} />
+        <Greeting name="kyunga" onButtonClick={handeleButtonClick}/> */}
     </div>
     
   )
